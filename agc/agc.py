a9a49dd75f6791eada472068ef52152adaad5b1c
AudreyyFortune <audefortune@free.fr>
2020-10-22 18:45:55 +0200

    in progress

--- agc/agc.py
+++ agc/agc.py
@@ -23,13 +23,13 @@ from collections import Counter
 # ftp://ftp.ncbi.nih.gov/blast/matrices/
 import nwalign3 as nw
 
-__author__ = "Your Name"
+__author__ = "Fortune Audrey"
 __copyright__ = "Universite Paris Diderot"
-__credits__ = ["Your Name"]
+__credits__ = ["Fortune Audrey"]
 __license__ = "GPL"
 __version__ = "1.0.0"
-__maintainer__ = "Your Name"
-__email__ = "your@email.fr"
+__maintainer__ = "Fortune Audrey"
+__email__ = "audefortune@free.fr"
 __status__ = "Developpement"
 
 
@@ -68,6 +68,109 @@ def get_arguments():
     parser.add_argument('-o', '-output_file', dest='output_file', type=str,
                         default="OTU.fasta", help="Output file")
     return parser.parse_args()
+
+
+def read_fasta(amplicon_file, minseqlen):
+	"""
+	Read a fasta file.
+	Parameters: - amplicon_file (.fasta)
+				- minseqlen (minimum length of sequences)
+	Return: a generator of sequences of size
+	"""
+	with gzip.open(amplicon_file, "rb") as filin:
+        for seq in filin:
+            if len(seq) >= minseqlen:
+                yield seq.strip()
+            continue
+
+
+def dereplication_fullength(amplicon_file, minseqlen, mincount):
+	"""
+	Determine unique sequences that occurs.
+	Parameters: - amplicon_file (.fasta)
+				- minseqlen (minimum length of sequences)
+				- mincount (minimum counting)
+	Return: a single sequence generator with occurrence [sequence, count]
+	"""
+	dict_occ = {}
+	for seq in read_fasta(amplicon_file, minseqlen):
+        if seq not in dict_occ.keys():    
+            dict_occ[seq] = 0
+        dict_occ[seq] += 1
+        #dict_occ[seq] += 1
+    new_dict_occ = dict(sorted(dict_occ.items(), key = lambda item : item[1], reverse = True))
+    for seq, count in new_dict_occ.items():
+        if count >= mincount:
+            yield [seq, count]
+	
+
+def get_chunk(seq, chunk_size):
+	"""
+	...
+	Parameters: - 
+				- 
+	Return: a chunk 
+	"""
+	chunk_list = []
+	for i in range(0, len(sequence), chunk_size):
+		if chunk_list:
+			if len(sequence[i:i + chunk_size]) != len(chunk_list[0]):
+				break
+		chunk_list.append(sequence[i:i + chunk_size])
+	if len(chunk_list) > 5:  # >=4
+		return chunk_list
+		
+
+def cut_kmer(seq, kmer_size):
+	"""
+	"""
+	pass
+
+
+def get_unique_kmer(kmer_dict, seq, id_seq, kmer_size):
+	"""
+	"""
+	pass
+	
+
+def search_mates(kmer_dict, seq, kmer_size):
+	"""
+	"""
+	pass
+	
+
+def get_identity(alignment_list):
+	"""
+	"""
+	pass
+
+
+def detect_chimera(perc_identity_matrix):
+	"""
+	"""
+	pass
+
+
+def chimera_removal(amplicon_file, minseqlen, mincount, chunk_size, kmer_size):
+	"""
+	"""
+	pass
+	
+
+def abundance_greedy_clustering(amplicon_file, minseqlen, mincount, chunk_size, kmer_size):
+	"""
+	"""
+	pass
+	
+
+def write_OTU(OTU_list, output_file):
+	"""
+	"""
+	pass
+
+
+
+
 
 #==============================================================
 # Main program
